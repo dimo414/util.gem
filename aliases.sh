b392355cbd7f1b5308263e91fe418c65f543205c
Michael Diamond@Aodh <dimo414@gmail.com>
2017-01-07 02:29:08 -0800

    Pulled in several aliases and functions from private gems

--- aliases.sh
+++ aliases.sh
@@ -2,3 +2,24 @@
 #
 # Bash aliases
 #
+
+alias dirsize='du -sh'
+alias diskspace='df -h'
+alias rot13='tr A-MN-Za-mn-z N-ZA-Mn-za-m'
+alias sortc='sort | uniq -c | sort -nr'
+[[ $OSTYPE == "darwin"* ]] && alias opentw='open -a TextWrangler'
+
+# Adds a 'clipboard' command to pipe into, that writes to the system clipboard
+#   command_whos_output_i_want_to_paste | clipboard
+# Calling clipboard on its own writes the current clipboard contents to stdout
+# See also http://williammitchell.blogspot.com/2008/03/fun-with-cygwins-devclipboard.html
+[[ -e /dev/clipboard ]] && alias clipboard="if_stdin 'cat > /dev/clipboard' 'cat /dev/clipboard'"  # Cygwin
+command -v pbcopy >& /dev/null && alias clipboard="if_stdin 'pbcopy' 'pbpaste'"                    # OSX
+command -v xclip >& /dev/null && alias clipboard="if_stdin 'xclip -sel clip' 'xclip -o -sel clip'" # Ubuntu
+
+# Adds an 'open' command which attempts to open the passed file with a GUI
+# program, such as a text editor.
+command -v cygstart >& /dev/null && alias open='cygstart'             # Cygwin
+command -v xdg-open >& /dev/null && alias open='xdg-open > /dev/null' # Ubuntu
+
+: # leave at the bottom to prevent the above && block from making this script return non-zero
--- functions.sh
+++ functions.sh
@@ -5,6 +5,117 @@
 # Loaded after environment.sh and aliases.sh
 #
 
+#
+# Shell Functions
+#
+
+# Takes two (generally sibling) commands to execute based on whether or not
+# stdin is being written to. $1 is executed if input is being piped in,
+# $2 is executed otherwise.
+# TODO just inline this, replacing aliases with functions?
+if_stdin() {
+  has_stdin="${1:?Must specify a command for stdin}"
+  shift
+  no_stdin="${1:?Must specify a command for no stdin}"
+  shift
+  # Need to use eval to support things like > redirection
+  if ! [[ -t 0 ]]; then
+    eval $has_stdin "$@"
+  else
+    eval $no_stdin "$@"
+  fi
+}
+
+# Buffers all output of the passed command, and only prints on error
+# Uses eval to support pipes and multiple commands, but defining a
+# wrapper function is generally recommended.
+# http://unix.stackexchange.com/a/41388/19157
+# TODO try http://joeyh.name/code/moreutils/ on Cygwin and just wrap chronic
+# if it's on the PATH.
+quiet_success() {
+  local output
+  output=$( eval "$@" 2>&1 )
+  local ret=$?
+  if (( ret != 0 )); then
+    echo "$output"
+    return $ret
+  fi
+}
+
+# Grep ps command
+# Inspiration: http://www.commandlinefu.com/commands/view/977/
+# Alternately: http://code.google.com/p/psgrep/
+# TODO how is this better than pgrep?
+# https://github.com/koalaman/shellcheck/wiki/SC2009
+psgrep() {
+  if [[ -z "$2" ]]
+  then
+    local psargs="aux"
+    local greparg="$1"
+  else
+    local psargs="$1"
+    local greparg="$2"
+  fi
+  # shellcheck disable=SC2001
+  greparg=$(echo "$greparg" | sed 's|\(.\)$|[\1]|')
+  # shellcheck disable=SC2086
+  ps $psargs | head -n 1
+  # shellcheck disable=SC2009,SC2086
+  ps $psargs | grep -i "$greparg"
+}
+
+# Send an email when the process passed as an argument terminates
+emailme() {
+  if [[ -z "$1" ]]
+  then
+    echo "Usage: emailme cmd cmdargs..."
+    echo "Usage: EMAIL=user@example.com emailme cmd cmdargs..."
+    return 0
+  fi
+
+  if [[ -z "$EMAIL" ]]
+  then
+    echo 'No EMAIL value found' >&2
+    # shellcheck disable=SC2119
+    emailme >&2 # usage
+    return 1
+  fi
+
+  local cmd=$1
+  shift
+  time $cmd "$@"
+
+  echo -e "From: $EMAIL\nTo: $EMAIL\nSubject: Finished running $cmd \n\nRan: $cmd $*\n\nStatus: $?" | sendmail -t
+  $_PGEM_DEBUG && echo "Emailed $EMAIL"
+}
+
+# Extracts archives of different types
+# http://www.shell-fu.org/lister.php?id=375
+extract () {
+  if [[ -f "$1" ]] ; then
+    case "$1" in
+      *.tar.bz2)   tar xvjf "$1"    ;;
+      *.tar.gz)    tar xvzf "$1"    ;;
+      *.bz2)       bunzip2 "$1"     ;;
+      *.rar)       unrar x "$1"     ;;
+      *.gz)        gunzip "$1"      ;;
+      *.tar)       tar xvf "$1"     ;;
+      *.tbz2)      tar xvjf "$1"    ;;
+      *.tgz)       tar xvzf "$1"    ;;
+      *.zip|*.jar) unzip "$1"       ;;
+      *.Z)         uncompress "$1"  ;;
+      *.7z)        7z x "$1"        ;;
+      *)
+        echo "'$1' cannot be extracted via extract"
+        return 2
+        ;;
+    esac
+  else
+    echo "'$1' is not a valid file"
+    return 1
+  fi
+}
+
 # Waits for processes that aren't a child process of this shell
 # Pair with:
 #   long_running_command & wait # this prints the PID of long_running_command
@@ -31,4 +142,31 @@ wait_port() {
     while ! listening "$1"; do sleep 1; done
     shift
   done
+}
+
+#
+# Git Functions
+#
+
+# Syncs a fork with the upstream repo
+# https://help.github.com/articles/syncing-a-fork/
+gitsyncfork() {
+  git fetch upstream &&
+  git checkout master &&
+  git merge upstream/master &&
+  echo "Ready to push with 'git push'"
+}
+
+#
+# Dev Functions
+#
+
+# Creates a Java pseudo-REPL for quick prototyping.
+# Launches an editor with a new Java file, then compiles and runs the file
+# after the editor exits.
+# Re-running in the same shell session opens the same file.
+java_demo() {
+  vi /tmp/Demo$$.java &&
+  echo "javac /tmp/Demo$$.java" >&2 && javac /tmp/Demo$$.java &&
+  echo "java -cp /tmp Demo$$" >&2   && java -cp /tmp Demo$$
 }
