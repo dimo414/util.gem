ad7d8bd9c1206eab49f200ee2a1d9ea056e0e1ad
Michael Diamond@Aodh <dimo414@gmail.com>
2017-02-15 02:04:24 -0800

    Restore unalias invocation.

--- aliases.sh
+++ aliases.sh
@@ -6,6 +6,7 @@
 alias dirsize='du -sh'
 alias diskspace='df -h'
 alias rot13='tr A-MN-Za-mn-z N-ZA-Mn-za-m'
+unalias screenopen 2>/dev/null # TODO delete by April 1 2017
 alias sortc='sort | uniq -c | sort -nr'
 
 # Adds a 'clipboard' command to pipe into, that writes to the system clipboard
