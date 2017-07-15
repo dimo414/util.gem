609b75df7645144e5366e5eb5ff4761d3b0d70d2
Michael Diamond <dimo414@gmail.com>
2017-07-15 13:22:10 -0700

    Consistently use &> instead of >&

--- aliases.sh
+++ aliases.sh
@@ -14,12 +14,12 @@ alias sortc='sort | uniq -c | sort -nr'
 # Calling clipboard on its own writes the current clipboard contents to stdout
 # See also http://williammitchell.blogspot.com/2008/03/fun-with-cygwins-devclipboard.html
 [[ -e /dev/clipboard ]] && alias clipboard="if_stdin 'cat > /dev/clipboard' 'cat /dev/clipboard'"  # Cygwin
-command -v pbcopy >& /dev/null && alias clipboard="if_stdin 'pbcopy' 'pbpaste'"                    # OSX
-command -v xclip >& /dev/null && alias clipboard="if_stdin 'xclip -sel clip' 'xclip -o -sel clip'" # Ubuntu
+command -v pbcopy &> /dev/null && alias clipboard="if_stdin 'pbcopy' 'pbpaste'"                    # OSX
+command -v xclip &> /dev/null && alias clipboard="if_stdin 'xclip -sel clip' 'xclip -o -sel clip'" # Ubuntu
 
 # Adds an 'open' command which attempts to open the passed file with a GUI
 # program, such as a text editor.
-command -v cygstart >& /dev/null && alias open='cygstart'             # Cygwin
-command -v xdg-open >& /dev/null && alias open='xdg-open > /dev/null' # Ubuntu
+command -v cygstart &> /dev/null && alias open='cygstart'             # Cygwin
+command -v xdg-open &> /dev/null && alias open='xdg-open > /dev/null' # Ubuntu
 
 : # leave at the bottom to prevent the above && block from making this script return non-zero
