494f174addb503b576b930784832dcf5b65a22b5
Michael Diamond@Aodh <dimo414@gmail.com>
2017-09-20 23:13:55 -0700

    Partial support for the clipboard alias in WSL. Copy works via clip.exe, but paste does not.
    Possible third-party solutions include:
    
    * https://github.com/equalsraf/win32yank
    * https://github.com/neosmart/paste (https://github.com/neovim/neovim/issues/7022)

--- aliases.sh
+++ aliases.sh
@@ -11,10 +11,12 @@ alias sortc='sort | uniq -c | sort -nr'
 # Adds a 'clipboard' command to pipe into, that writes to the system clipboard
 #   command_whos_output_i_want_to_paste | clipboard
 # Calling clipboard on its own writes the current clipboard contents to stdout
-command -v pbcopy &> /dev/null && alias clipboard="if_stdin 'pbcopy' 'pbpaste'"                    # OSX
-command -v xclip &> /dev/null && alias clipboard="if_stdin 'xclip -sel clip' 'xclip -o -sel clip'" # Ubuntu
+command -v pbcopy &> /dev/null && alias clipboard="if_stdin 'pbcopy' 'pbpaste'"                          # OSX
+command -v xclip &> /dev/null && alias clipboard="if_stdin 'xclip -sel clip' 'xclip -o -sel clip'"       # Ubuntu
+# WSL must come before Cygwin, since Cygwin also has clip.exe on the path
+command -v clip.exe &> /dev/null && alias clipboard="if_stdin 'clip.exe' 'pgem_err paste not supported'" # WSL
 # See also http://williammitchell.blogspot.com/2008/03/fun-with-cygwins-devclipboard.html
-[[ -e /dev/clipboard ]] && alias clipboard="if_stdin 'cat > /dev/clipboard' 'cat /dev/clipboard'"  # Cygwin
+[[ -e /dev/clipboard ]] && alias clipboard="if_stdin 'cat > /dev/clipboard' 'cat /dev/clipboard'"        # Cygwin
 
 # Adds an 'open' command which attempts to open the passed file with a GUI
 # program, such as a text editor.
