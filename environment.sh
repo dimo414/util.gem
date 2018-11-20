c7e193a49b69e2ff382425cd2d8cdee863b65c6c
Michael Diamond <dimo414@gmail.com>
2018-11-20 14:40:34 -0800

    Include a pg::require call for fzf, since it's used by goto.

--- environment.sh
+++ environment.sh
@@ -3,3 +3,6 @@
 # Update, specify, and compute environment variables, such as
 # PATH, or PS1 here.
 #
+
+# External commands
+pg::require fzf "Installation instructions: https://github.com/junegunn/fzf"
